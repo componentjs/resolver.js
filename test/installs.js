b1bc89802b6711e3a865cff80114f30e494c1d1c
Jonathan Ong <jonathanrichardong@gmail.com>
2014-02-08 09:49:44 -0800

    change component download folders to user/repo/ref

--- test/installs.js
+++ test/installs.js
@@ -32,7 +32,7 @@ describe('Installer', function () {
       }
     }, options)
     var tree = yield* resolver.tree()
-    var dir = join(components, 'component-query-0.0.2')
+    var dir = join(components, 'component', 'query', '0.0.2')
     fs.statSync(join(dir, 'index.js'))
     fs.statSync(join(dir, 'component.json'))
   }))
@@ -44,15 +44,15 @@ describe('Installer', function () {
       }
     }, options)
     var tree = yield* resolver.tree()
-    fs.statSync(join(components, 'component-classes-1.2.0', 'component.json'))
-    fs.statSync(join(components, 'component-indexof-0.0.2', 'component.json'))
+    fs.statSync(join(components, 'component', 'classes', '1.2.0', 'component.json'))
+    fs.statSync(join(components, 'component', 'indexof', '0.0.2', 'component.json'))
   }))
 
   it('should install simple-dependencies', co(function* () {
     var resolver = new Resolver(fixture('simple-dependencies'), options)
     var tree = yield* resolver.tree()
-    fs.statSync(join(components, 'component-emitter-1.1.1', 'component.json'))
-    fs.statSync(join(components, 'component-domify-1.1.1', 'component.json'))
+    fs.statSync(join(components, 'component', 'emitter', '1.1.1', 'component.json'))
+    fs.statSync(join(components, 'component', 'domify', '1.1.1', 'component.json'))
   }))
 
   it('should install font-awesome and not care about casing', co(function* () {
@@ -61,7 +61,7 @@ describe('Installer', function () {
         'FortAwesome/Font-Awesome': '4.0.3'
       }
     }, options)
-    var out = join(components, 'fortawesome-font-awesome-v4.0.3')
+    var out = join(components, 'fortawesome', 'font-awesome', 'v4.0.3')
     var tree = yield* resolver.tree()
     fs.statSync(join(out, 'component.json'))
     fs.statSync(join(out, 'css', 'font-awesome.css'))
@@ -75,7 +75,7 @@ describe('Installer', function () {
       }
     }, options)
     var tree = yield* resolver.tree()
-    var out = join(components, 'component-test-glob-0.0.1')
+    var out = join(components, 'component-test', 'glob', '0.0.1')
     fs.statSync(join(out, 'lib', 'index.js'))
     fs.statSync(join(out, 'lib', 'index.css'))
     var json = require(join(out, 'component.json'))
